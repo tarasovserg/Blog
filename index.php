6c3dfa30a22ee9f210b0b4416bb8655d5c041621
tarasovserg <tarasovsergserg@gmail.com>
2013-12-14 20:30:02 +0200

    -tss last

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
+    ob_start();
     session_start();
     require_once 'views/header.html';
     require 'views/menu.php';
     require 'views/body.php';
     require 'views/footer.php';
-    
 ?>
 
